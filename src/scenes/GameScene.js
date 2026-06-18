9dcc62137ce179d99b41a21875cb590fabc63777
Artur Salgado <artursalgado00@gmail.com>
2026-06-18 19:32:46 +0100

    feat: musica de fundo em loop durante o jogo (#14)
    - GameScene.create(): startBgMusic() apos resume() -- musica arranca
      ao entrar no jogo (drone ambient + vento procedural ja existia no
      SoundManager mas nunca era chamado durante o jogo)
    - GameOverScene.create(): stopBgMusic() antes do som 'die' -- garante
      que a musica para correctamente ao morrer
    - VictoryScene ja chamava stopBgMusic(), MenuScene ja chamava startBgMusic()

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -77,6 +77,7 @@ export default class GameScene extends Phaser.Scene {
 
     create() {
         SoundManager.resume();
+        SoundManager.startBgMusic();
 
         // Reset estado
         this._elapsedSec   = 0;
